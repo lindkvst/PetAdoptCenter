ba3e0ae6c15be084fb7d6ac6f568ea9afcc830aa
AlexArnold01 <alar1000@stud.ek.dk>
2025-09-15 15:05:42 +0200

    lavet videre på ScannerHelper

--- src/PetAdopt/ScannerHelper.java
+++ src/PetAdopt/ScannerHelper.java
@@ -3,11 +3,11 @@ package PetAdopt;
 import java.util.Scanner;
 
 public class ScannerHelper {
-    private Scanner sc;
-
-    public ScannerHelper() {
-        this.sc = new Scanner(System.in);
-    }
+//    private Scanner sc;
+//
+//    public ScannerHelper() {
+//        this.sc = new Scanner(System.in);
+//    }
 
     public void welcomeMessage() {
         System.out.println("Welcome to the Pet Adoption Center");
@@ -17,15 +17,36 @@ public class ScannerHelper {
     public int registerPets() {
         Scanner sc = new Scanner(System.in);
         System.out.println("How many pets do you want to register? ");
-        int NumOfPets = sc.nextInt();
-        return NumOfPets;
+        int numOfPets = sc.nextInt();
+        return numOfPets;
     }
 
     public String askType() {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type (dog/cat/lizard): ");
+        String petType = sc.nextLine();
+        boolean correctAnswer = false;
+        while (!correctAnswer) {
+            if (petType.equals("dog")) {
+                return petType;
+                correctAnswer = true;
+                break;
+            } else if (petType.equals("cat")) {
+                return petType;
+                correctAnswer = true;
+                break;
+            } else if (petType.equals("lizard")) {
+                return petType;
+                correctAnswer = true;
+                break;
+            } else {
+                System.out.println("Sorry, I didn't quite get that. Please enter a valid type");
+            }
+        }
+        return
     }
 
     public String askName() {
-
+        System.out.println("Name: " + );
     }
 }
