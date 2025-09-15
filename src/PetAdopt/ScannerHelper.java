f7be5594f1306fbb8c141e96909eca79a2ca7960
AlexArnold01 <alar1000@stud.ek.dk>
2025-09-15 14:26:03 +0200

    lavet videre på ScannerHelper

--- src/PetAdopt/ScannerHelper.java
+++ src/PetAdopt/ScannerHelper.java
@@ -4,5 +4,19 @@ import java.util.Scanner;
 
 public class ScannerHelper {
     private Scanner sc;
-}
 
+    public ScannerHelper (Scanner sc) {
+        this.sc = new Scanner(System.in);
+    }
+}
+    public void welcomeMessage() {
+        System.out.println("Welcome to the Pet Adoption Center");
+        System.out.println("Let's register some animals!");
+    }
+
+    public int registerPets() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many pets do you want to register? ");
+        int registerNumOfPets = sc.nextInt();
+        return registerNumOfPets;
+    }
