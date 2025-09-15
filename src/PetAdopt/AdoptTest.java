3d6895a807e08adc24bd73b5f9845f38abe151c1
lindkvst <lindkviste@gmail.com>
2025-09-15 14:02:33 +0200

    02 Test

--- src/PetAdopt/AdoptTest.java
+++ src/PetAdopt/AdoptTest.java
@@ -1,4 +1,12 @@
 package PetAdopt;
 
 public class AdoptTest {
+
+    public static void main(String[] args) {
+        RunAdoption();
+    }
+
+    public static void RunAdoption() {
+        //something
+    }
 }
